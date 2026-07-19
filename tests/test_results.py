1500e1200611bb9ce91854feaed118d582b93bb2
trulyaldi <aldiyar.moldazhanov@nu.edu.kz>
2026-07-19 14:29:36 +0500

    add result upsert test

---
+++ tests/test_results.py
@@ -0,0 +1,27 @@
+from database.results import save_result, get_result
+
+
+def test_save_result_updates_existing():
+    result_id = save_result(
+        "PYTEST_SYSTEM",
+        "energy",
+        "exp",
+        "demo",
+        -1.0,
+        uncertainty=0.1,
+    )
+
+    updated_id = save_result(
+        "PYTEST_SYSTEM",
+        "energy",
+        "exp",
+        "demo",
+        -2.0,
+        uncertainty=0.2,
+    )
+
+    result = get_result(updated_id)
+
+    assert updated_id == result_id
+    assert result["extrapolated_value"] == -2.0
+    assert result["uncertainty"] == 0.2
